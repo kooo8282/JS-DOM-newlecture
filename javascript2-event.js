800fc15ac5266e24de85f8d1d3441b0680cd6c21
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2020-11-16 16:41:33 +0900

    event bubbling

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,3 +1,37 @@
+// Ex 2-1 - delete row
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section2-1");    
+    var tbody = section.querySelector("tbody");
+
+    tbody.onclick = function(e){
+        if (e.target.nodeName != "INPUT") return;
+        var rowSelected = e.target.parentElement.parentElement;
+        rowSelected.remove();
+    }    
+});
+
+// Ex 2 - Show Image selected: event bubbling
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section2");
+    var imgList = section.querySelector(".img-list");
+    // var imgs = section.querySelectorAll(".img");
+    var currentImg = section.querySelector(".current-img");
+
+    imgList.onclick = function(e){        
+        // img가 아니면 아무짓도 하지마라.
+        if(e.target.nodeName != "IMG") return;
+        // 선택된 img src를 대입하라.
+        currentImg.src = e.target.src;
+    }
+
+
+    // for(var i=0; i<imgs.length; i++){
+    //     imgs[i].onclick = function(e){
+    //         currentImg.src = e.target.src;
+    //     }
+    // }
+});
+
 // Ex 1-1 - delete row
 window.addEventListener("load", function(){
     var section = document.querySelector("#section1-1");    
