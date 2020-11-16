ab0a5d5f3a7162efb5eb0995c75cb2d3a995f57d
kooo8282 <kooo8282@gmail.com>
2020-11-16 23:46:30 +0900

    moving 3boxes by page-offset-left

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,3 +1,36 @@
+// EX 9 - MouseEvent Position: moving 3 boxes By page-offset-left
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section9");
+    var container = section.querySelector(".container");
+    var status = section.querySelector(".status");
+    var dragging = false;
+    var offset = {x:0,y:0}
+    var current = null;
+    var left = container.offsetLeft;
+    var top = container.offsetTop;
+    console.log(left, top)
+
+    container.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;
+            offset.x = e.offsetX;
+            offset.y = e.offsetY;
+        }
+    };
+    container.onmousemove = function(e){
+        if(!dragging){return;}
+        var x = e.pageX-offset.x-left
+        var y = e.pageY-offset.y-top
+        current.style.left = x+"px";
+        current.style.top = y+"px";
+        status.innerText = "(x, y):("+x+","+y+")";
+    };
+    container.onmouseup = function(e){
+        dragging = false;
+    };    
+});
+
 // EX 8 - MouseEvent Position: moving 3 boxes By Drag
 window.addEventListener("load", function(){
     var section = document.querySelector("#section8");
@@ -22,8 +55,7 @@ window.addEventListener("load", function(){
     };
     container.onmouseup = function(e){
         dragging = false;
-    };
-    
+    };    
 });
 
 // EX 7 - MouseEvent Position: By Drag
