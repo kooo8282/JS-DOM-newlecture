70d0e3fef33b25b9084c3dc8130919021d98e2ca
kooo8282 <kooo8282@gmail.com>
2020-11-16 23:32:21 +0900

    moving 3boxes by Dragging

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,3 +1,31 @@
+// EX 8 - MouseEvent Position: moving 3 boxes By Drag
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section8");
+    var container = section.querySelector(".container");
+    var box = section.querySelector(".box");
+    var dragging = false;
+    var offset = {x:0,y:0}
+    var current = null;
+
+    container.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;
+            offset.x = e.offsetX;
+            offset.y = e.offsetY;
+        }
+    };
+    container.onmousemove = function(e){
+        if(!dragging){return;}
+        current.style.left = e.pageX-offset.x+"px";
+        current.style.top = e.pageY-offset.y+"px";
+    };
+    container.onmouseup = function(e){
+        dragging = false;
+    };
+    
+});
+
 // EX 7 - MouseEvent Position: By Drag
 window.addEventListener("load", function(){
     var section = document.querySelector("#section7");
