93335f77eeaac14b97b623d4e1faf6b6d079da8e
kooo8282 <kooo8282@gmail.com>
2020-11-16 23:22:17 +0900

    box moving by Drag

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,3 +1,29 @@
+// EX 7 - MouseEvent Position: By Drag
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section7");
+    var container = section.querySelector(".container");
+    var box = section.querySelector(".box");
+    var dragging = false;
+    var offset = {x:0,y:0}
+    container.onmousedown = function(e){
+        if(e.target===box){
+            dragging = true;
+        }
+    };
+    container.onmousemove = function(e){
+        if(!dragging){return;}
+        box.style.left = e.pageX-offset.x+"px";
+        box.style.top = e.pageY-offset.y+"px";
+    };
+    container.onmouseup = function(e){
+        dragging = false;
+    };
+    box.onmousedown = function(e){
+        offset.x = e.offsetX;
+        offset.y = e.offsetY;
+    }
+});
+
 // EX 6 - MouseEvent Position
 window.addEventListener("load", function(){
     var section = document.querySelector("#section6");
